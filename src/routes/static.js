5a9951869242c9e368855f96c4a8fb270db7d4bb
elizzzabethrae <elizabethwarners@gmail.com>
2019-02-16 13:32:33 -0500

    deploy to production

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
+  res.send("Welcome to See Spot");
 });
 
 module.exports = router;
