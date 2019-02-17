50a52302459342240e7dd2394e5dcba21c2b6917
elizzzabethrae <elizabethwarners@gmail.com>
2019-02-17 11:18:15 -0500

    cleanup

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController")
 
-//router.get("/posts", postController.index);
 router.get("/posts/lost", postController.lost);
 router.get("/posts/found", postController.found);
 router.get("/posts/new", postController.new);
